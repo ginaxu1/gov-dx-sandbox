7cb97a92b50a8f628bc83035cb29398fdb1598f8
mushrafmim <mushrafmim@gmail.com>
2025-08-13 22:39:54 +0530

    update configs to match deployment configs

--- graphql-resolver/index.js
+++ graphql-resolver/index.js
@@ -10,10 +10,10 @@ const gateway = new ApolloGateway({
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
       // This is your "Registry of Persons" service providing user name
-      { name: 'drp', url: process.env.CHOREO_DRP_CONNECTION_1_SERVICEURL || 'http://localhost:9090/', headers: { "Choreo-API-Key": process.env.CHOREO_DRP_CONNECTION_1_APIKEY } },
+      { name: 'drp', url: process.env.CHOREO_DRP_CONNECTION_SERVICEURL || 'http://localhost:9090/', headers: { "Choreo-API-Key": process.env.CHOREO_DRP_CONNECTION_APIKEY } },
 
       // This is your "Department of Motor Traffic" service providing license info
-      { name: 'dmt', url: process.env.CHOREO_DMT_CONNECTION_1_SERVICEURL || 'http://localhost:9091/', headers: { "Choreo-API-Key": process.env.CHOREO_DMT_CONNECTION_1_APIKEY } },
+      { name: 'dmt', url: process.env.CHOREO_DMT_CONNECTION_SERVICEURL || 'http://localhost:9091/', headers: { "Choreo-API-Key": process.env.CHOREO_DMT_CONNECTION_APIKEY } },
     ],
     // Optional: Set a poll interval to refresh the schema every 10 seconds
     // pollIntervalInMs: 10000, 
