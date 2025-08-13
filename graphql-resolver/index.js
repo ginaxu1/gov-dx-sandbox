1ce45a0da41aeadcfd5f09aa4db095cbb81d3d4d
Your Name <regina.xu1@gmail.com>
2025-08-13 11:34:46 +0530

    update dmt component and graphql-resolver URL

--- graphql-resolver/index.js
+++ graphql-resolver/index.js
@@ -10,10 +10,10 @@ const gateway = new ApolloGateway({
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
       // This is your "Registry of Persons" service providing user name
-      { name: 'drp', url: process.env.DRP_API_URL || 'http://localhost:9090/' },
+      { name: 'drp', url: process.env.CHOREO_DRP_CONNECTION_SERVICEURL || 'http://localhost:9090/' },
 
       // This is your "Department of Motor Traffic" service providing license info
-      { name: 'dmt', url: process.env.DMT_API_URL || 'http://localhost:9091/' },
+      { name: 'dmt', url: process.env.CHOREO_DMT_CONNECTION_SERVICEURL || 'http://localhost:9091/' },
     ],
     // Optional: Set a poll interval to refresh the schema every 10 seconds
     // pollIntervalInMs: 10000, 
