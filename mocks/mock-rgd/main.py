d0150a5d3f01776993be59febfda752e9b6c9e54
mushrafmim <mushrafmim@gmail.com>
2025-08-21 17:05:15 +0530

    running on public ip

--- mocks/mock-rgd/main.py
+++ mocks/mock-rgd/main.py
@@ -144,6 +144,7 @@ port = int(os.getenv("PORT", 8080))
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
+        host="0.0.0.0",
         port=8080,
         reload=True,
         log_level="info"
