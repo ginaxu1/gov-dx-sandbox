16002585ba5b29c7b1ae1cbb2cbff98d58751e7c
mushrafmim <mushrafmim@gmail.com>
2025-08-22 11:12:21 +0530

    removing dmt connection

--- graphql-resolver/index.js
+++ graphql-resolver/index.js
@@ -15,7 +15,6 @@ const gateway = new ApolloGateway({
       { name: 'drp', url: process.env.CHOREO_DRP_CONNECTION_SERVICEURL || 'http://localhost:9090/' },
 
       // This is your "Department of Motor Traffic" service providing license info
-      { name: 'dmt', url: process.env.CHOREO_DMT_CONNECTION_SERVICEURL || 'http://localhost:9091/' },
       { name: 'rgd', url: process.env.CHOREO_RGD_CONNECTION_SERVICEURL || 'http://localhost:9092/' },
     ],
     // Optional: Set a poll interval to refresh the schema every 10 seconds
@@ -30,12 +29,6 @@ const gateway = new ApolloGateway({
           request.http?.headers.set(
             'Choreo-API-Key',
             process.env.CHOREO_DRP_CONNECTION_CHOREOAPIKEY ?? ''
-          );
-        }
-        if (name === 'dmt') {
-          request.http?.headers.set(
-            'Choreo-API-Key',
-            process.env.CHOREO_DMT_CONNECTION_CHOREOAPIKEY ?? ''
           );
         }
         if (name === 'rgd') {
