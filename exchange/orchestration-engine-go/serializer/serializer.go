c3d7fe68dc3a5fb1cb14a6ac2cf5b8847b457c2f
mushrafmim <mushrafmim@gmail.com>
2025-09-02 09:55:42 +0530

    fix: Rewrite Serializer Logs in a better way

--- exchange/orchestration-engine-go/serializer/serializer.go
+++ exchange/orchestration-engine-go/serializer/serializer.go
@@ -2,7 +2,7 @@ package serializer
 
 import "fmt"
 
-// Serializer is responsible for serializing the standard query to raw query and the response back to standard response.
+// Serializer is responsible for converting the standard query to raw query and raw query response to standard response.
 func Serializer() {
 	fmt.Println("This is the serializer")
 }
