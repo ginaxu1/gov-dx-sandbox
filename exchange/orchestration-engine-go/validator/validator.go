2576a70760bba526acc3f4dbb2751ee2fa3fa25c
mushrafmim <mushrafmim@gmail.com>
2025-09-02 09:57:27 +0530

    fix: Grammar issue

--- exchange/orchestration-engine-go/validator/validator.go
+++ exchange/orchestration-engine-go/validator/validator.go
@@ -2,7 +2,7 @@ package validator
 
 import "fmt"
 
-// Validate act as a middleware to validate the incoming request against the policy and the consent requirements.
+// Validate acts as a middleware to validate the incoming request against the policy and the consent requirements.
 func Validate() {
 	fmt.Println("This is the main validator")
 }
