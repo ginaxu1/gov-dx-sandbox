8d569e0fb494da20fe87345fc8d9c06d06b823ac
mushrafmim <mushrafmim@gmail.com>
2025-09-02 10:19:53 +0530

    feat: initial working setup

--- exchange/orchestration-engine-go/server/server.go
+++ exchange/orchestration-engine-go/server/server.go
@@ -6,13 +6,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/ginaxu1/gov-dx-sandbox/federator"
 )
 
 type Response struct {
 	Message string `json:"message"`
 }
 
-func RunServer() {
+func RunServer(f *federator.Federator) {
 	// /health route
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
@@ -22,6 +24,27 @@ func RunServer() {
 		resp := Response{Message: "OpenDIF Server is Healthy!"}
 		w.Header().Set("Content-Type", "application/json")
 		err := json.NewEncoder(w).Encode(resp)
+		if err != nil {
+			return
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		// Parse request body
+		var req federator.GraphQLRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "bad request: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		response := f.HandleQuery(req)
+
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(response)
 		if err != nil {
 			return
 		}
