8ec4303a004340e1599ae55bcac5b45fb4b7b2f6
Thanikan Sivatheepan <sthanikan2000@gmail.com>
2025-09-03 12:34:07 +0530

    Template directory for the portals created (#43)

---
+++ portals/template-portal/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-5xl font-bold">Welcome to the Template Portal</h1>
+    </div>
+  )
+}
+
+export default App
