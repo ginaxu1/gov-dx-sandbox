6eba3e32e3db0558001ec29318ef061cb0a4a5af
mushrafmim <mushrafmim@gmail.com>
2025-09-03 10:25:07 +0530

    fix: update config file providers list to match key values

--- exchange/orchestration-engine-go/pkg/provider/Provider.go
+++ exchange/orchestration-engine-go/pkg/provider/Provider.go
@@ -2,7 +2,7 @@ package provider
 
 // Provider struct that represents a provider information.
 type Provider struct {
-	ServiceUrl string `json:"serviceUrl,omitempty"`
-	ServiceKey string `json:"serviceKey,omitempty"`
+	ServiceUrl string `json:"providerUrl,omitempty"`
+	ServiceKey string `json:"providerKey,omitempty"`
 	ApiKey     string `json:"apiKey,omitempty"`
 }
