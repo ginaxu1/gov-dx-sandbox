d2d0bab4bad86ca19134e88d695c3eef917bedfb
mushrafmim <mushrafmim@gmail.com>
2025-09-10 17:46:38 +0530

    feat: add initial working setup of OE.

--- exchange/orchestration-engine-go/configs/config.go
+++ exchange/orchestration-engine-go/configs/config.go
@@ -5,17 +5,42 @@ import (
 	"os"
 
 	"github.com/ginaxu1/gov-dx-sandbox/exchange/orchestration-engine-go/pkg/federator"
+	"github.com/ginaxu1/gov-dx-sandbox/exchange/orchestration-engine-go/pkg/graphql"
+	"github.com/graphql-go/graphql/language/ast"
+	"github.com/graphql-go/graphql/language/parser"
+	"github.com/graphql-go/graphql/language/source"
 )
 
 // Cfg defines the configuration structure for the application.
 type Cfg struct {
 	*federator.Options
+	*graphql.MappingAST
+	Schema *ast.Document
 }
 
 const ConfigFilePath = "./config.json"
+const SDLFilePath = "./schema.graphql"
 
 // AppConfig is a global variable to hold the application configuration.
 var AppConfig *Cfg
+
+func LoadSdlSchema() *ast.Document {
+	schema, err := os.ReadFile(SDLFilePath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Wrap as a GraphQL source
+	src := source.NewSource(&source.Source{
+		Body: schema,
+		Name: "SchemaSDL",
+	})
+
+	doc, err := parser.Parse(parser.ParseParams{Source: src})
+
+	return doc
+}
 
 // LoadConfig reads the configuration from the config.json file and unmarshal it into the AppConfig variable.
 func LoadConfig() {
@@ -33,20 +58,9 @@ func LoadConfig() {
 
 	err = json.Unmarshal(file, AppConfig)
 
+	AppConfig.Schema = LoadSdlSchema()
+
 	if err != nil {
 		panic(err)
 	}
-}
-
-func IsProviderExists(providerKey string) bool {
-	if AppConfig == nil || AppConfig.Options == nil {
-		return false
-	}
-
-	for _, p := range AppConfig.Options.Providers {
-		if p.ServiceKey == providerKey {
-			return true
-		}
-	}
-	return false
 }
