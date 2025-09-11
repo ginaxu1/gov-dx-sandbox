df0d77140d42a8c85b279ba4fef6cd7eb79f0265
ginaxu1 <167130561+ginaxu1@users.noreply.github.com>
2025-09-11 09:43:08 +0530

    Delete api-server directory

--- api-server/src/database.ts
+++
@@ -1,8 +0,0 @@
-import { Application, ProviderProfile, ProviderSchema, ProviderSubmission } from "./types";
-
-// IN-MEMORY DATABASES
-// TODO: in v2, replace with persistent database like PostgreSQL
-export const applicationsDB: Application[] = [];
-export const providerProfilesDB: ProviderProfile[] = [];
-export const providerSchemasDB: ProviderSchema[] = [];
-export const providerSubmissionsDB: ProviderSubmission[] = [];
