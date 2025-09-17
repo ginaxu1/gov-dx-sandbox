771893fc82eabbcc1980ccd29b5269ea6520bd56
Thanikan <sthanikan2000@gmail.com>
2025-09-17 23:38:28 +0530

    intial commit

--- portals/consent-portal/src/vite-env.d.ts
+++ portals/consent-portal/src/vite-env.d.ts
@@ -4,6 +4,10 @@ interface ImportMetaEnv {
   readonly VITE_CONSENT_ENGINE_PATH: string;
   readonly VITE_BASE_PATH: string;
   readonly VITE_PORT: string;
+  readonly VITE_CLIENT_ID: string;
+  readonly VITE_BASE_URL: string;
+  readonly VITE_SCOPE: string;
+  // more env variables...
 }
 
 interface ImportMeta {
