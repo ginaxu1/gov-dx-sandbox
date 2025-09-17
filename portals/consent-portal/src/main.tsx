0c1ac88412bfab05a5a05ac9676e19016369a5af
Thanikan <sthanikan2000@gmail.com>
2025-09-17 23:56:07 +0530

    feat: update Window interface to require configs and use dynamic redirect URLs

--- portals/consent-portal/src/main.tsx
+++ portals/consent-portal/src/main.tsx
@@ -6,17 +6,19 @@ import { AuthProvider } from "@asgardeo/auth-react";
 
 declare global {
   interface Window {
-    configs?: {
+    configs: {
       apiUrl: string;
       VITE_CLIENT_ID: string;
       VITE_BASE_URL: string;
       VITE_SCOPE: string;
+      signInRedirectURL: string;
+      signOutRedirectURL: string;
     };
   }
 }
 const config = {
-     signInRedirectURL: "http://localhost:5173",
-     signOutRedirectURL: "http://localhost:5173",
+     signInRedirectURL: window?.configs?.signInRedirectURL || "http://localhost:5173",
+     signOutRedirectURL: window?.configs?.signOutRedirectURL || "http://localhost:5173",
      clientID: window?.configs?.VITE_CLIENT_ID || import.meta.env.VITE_CLIENT_ID,
      baseUrl: window?.configs?.VITE_BASE_URL || import.meta.env.VITE_BASE_URL,
      scope: window?.configs?.VITE_SCOPE ? window.configs.VITE_SCOPE.split(',') : import.meta.env.VITE_SCOPE?.split(',') || ['openid', 'profile']
