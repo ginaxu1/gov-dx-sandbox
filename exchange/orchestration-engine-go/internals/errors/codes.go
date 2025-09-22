892406b3c83532bd857fa53eb77af58081e86f2b
mushrafmim <mushrafmim@gmail.com>
2025-09-22 14:44:20 +0530

    feat: decode header and push appId and subscriberId

---
+++ exchange/orchestration-engine-go/internals/errors/codes.go
@@ -0,0 +1,31 @@
+package errors
+
+// PDP-related
+const (
+	CodePDPNotAllowed  = "PDP_NOT_ALLOWED"
+	CodePDPUnavailable = "PDP_UNAVAILABLE"
+	CodePDPError       = "PDP_ERROR"
+	CodePDPNoResponse  = "PDP_NO_RESPONSE"
+)
+
+// CE-related
+const (
+	CodeCEUnavailable    = "CE_UNAVAILABLE"
+	CodeCEError          = "CE_ERROR"
+	CodeCENoResponse     = "CE_NO_RESPONSE"
+	CodeCEConsentDenied  = "CE_CONSENT_DENIED"
+	CodeCEConsentExpired = "CE_CONSENT_EXPIRED"
+	CodeCENotApproved    = "CE_NOT_APPROVED"
+)
+
+// Auth-related
+const (
+	CodeUnauthorized = "UNAUTHORIZED"
+	CodeForbidden    = "FORBIDDEN"
+)
+
+// Generic
+const (
+	CodeInternalError = "INTERNAL_ERROR"
+	CodeBadRequest    = "BAD_REQUEST"
+)
